e0a7fcf0ba4752410e98a500f1867e161aa7cd4e
Takumi KANEMASA <113566763+k21rs034@users.noreply.github.com>
2023-06-05 15:02:43 +0900

    休みを出力する

--- holiday.php
+++ holiday.php
@@ -1,5 +1,14 @@
 <?php
 
 function getHoliday($year){
-    
+    $holiday = [
+        '1-1' => '元日',
+        '2-23' => '天皇誕生日',
+        '5-4' => 'みどりの日',
+        '5-5' => 'こどもの日'
+    ];
+    return $holiday;
 }
+
+$holiday = getHoliday(2024);
+print_r($holiday);
