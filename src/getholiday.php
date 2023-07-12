a77a913be9605d769bb06e81c6e81d16e41bd134
Takumi KANEMASA <113566763+k21rs034@users.noreply.github.com>
2023-07-12 11:39:29 +0900

    完成
    仕様の記述と各関数の完成

--- src/getholiday.php
+++ src/getholiday.php
@@ -1,58 +1,137 @@
 <?php
- if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
-  $y = $_POST['year'];
-  $m = $_POST['month'];
-  $d = $_POST['day'];
+function calculateCalendar($y, $m, $d)
+{
   $date = $y . '-' . $m . '-' . $d;
   $md1 = date('t', strtotime($date));
 
-  $spHoliday = [
-    '1-1' => '元日',
-    '1-2' => '振替休日',
-    '1-9' => '成人の日',
-    '2-11' => '建国記念の日',
-    '2-23' => '天皇誕生日',
-    '3-21' => '春分の日',
-    '4-29' => '昭和の日',
-    '5-3' => '憲法記念日',
-    '5-4' => 'みどりの日',
-    '5-5' => 'こどもの日',
-    '7-17' => '海の日',
-    '8-11' => '山の日',
-    '9-18' => '敬老の日',
-    '9-23' => '秋分の日',
-    '10-9' => 'スポーツの日',
-    '11-3' => '文化の日',
+  $result = [
+    'totalDays' => $md1,
+    'weekday' => getWeekday($date),
+    'calendar' => []
+  ];
+
+  //$firstDayOfMonth = date("w", mktime(0, 0, 0, $m, 1, $y));
+  $lastDayOfMonth = date("t", mktime(0, 0, 0, $m, 1, $y));
+
+  // 日にちと曜日の計算
+  for ($day = 1; $day <= $lastDayOfMonth; $day++) {
+    $currentDayOfWeek = date("w", mktime(0, 0, 0, $m, $day, $y));
+    $dayOfWeek = ['日', '月', '火', '水', '木', '金', '土'];
+
+    $holidayName = getHolidayName($y, $m, $day);
+
+    $result['calendar'][] = [
+      'day' => $day,
+      'weekday' => $dayOfWeek[$currentDayOfWeek],
+      'holiday' => $holidayName
+    ];
+  }
+
+  return $result;
+}
+
+function getWeekday($date)
+{
+  $weekday = date('w', strtotime($date));
+  $weekdays = ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'];
+  return $weekdays[$weekday];
+}
+
+function getHolidayName($year, $month, $day)
+{
+  $holidayName = '';
+
+  // 固定祝日
+  $fixedHolidays = [
+    '01-01' => '元日',
+    '02-11' => '建国記念の日',
+    '02-23' => '天皇誕生日',
+    '04-29' => '昭和の日',
+    '05-03' => '憲法記念日',
+    '05-04' => 'みどりの日',
+    '05-05' => 'こどもの日',
+    '08-11' => '山の日',
+    '11-03' => '文化の日',
     '11-23' => '勤労感謝の日'
   ];
 
-  echo "<br>{$y}年{$m}月は{$md1}日あります。";
+  $date = date('m-d', mktime(0, 0, 0, $month, $day, $year));
 
-  function getWeekday($date)
-  {
-    $weekday = date('w', strtotime($date));
-    $weekdays = ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'];
-    return $weekdays[$weekday];
+  if (isset($fixedHolidays[$date])) {
+    $holidayName = $fixedHolidays[$date];
+  } else {
+    // 春分の日の計算
+    $springEquinox = floor(20.8431 + 0.242194 * ($year - 1980)) - floor(($year - 1980) / 4);
+    $springEquinoxTimestamp = mktime(0, 0, 0, 3, 21 + $springEquinox, $year);
+    $springEquinoxDate = date('m-d', $springEquinoxTimestamp);
+
+    // 秋分の日の計算
+    $autumnEquinox = floor(23.2488 + 0.242194 * ($year - 1980)) - floor(($year - 1980) / 4);
+    $autumnEquinoxTimestamp = mktime(0, 0, 0, 9, 23 + $autumnEquinox, $year);
+    $autumnEquinoxDate = date('m-d', $autumnEquinoxTimestamp);
+
+    // 第2月曜日を計算
+    $secondMonday = strtotime("second monday of $year-$month");
+    $secondMondayDate = date('m-d', $secondMonday);
+
+    // 第3月曜日を計算
+    $thirdMonday = strtotime("third monday of $year-$month");
+    $thirdMondayDate = date('m-d', $thirdMonday);
+
+    // 成人の日の計算（1月の場合）
+    if ($month == '01' && $date == $secondMondayDate) {
+      $holidayName = '成人の日';
+    }
+
+    // 海の日の計算
+    if ($month == '07' && $date == $thirdMondayDate) {
+      $holidayName = '海の日';
+    }
+
+    // 敬老の日の計算（9月の場合）
+    if ($month == '09' && $date == $thirdMondayDate) {
+      $holidayName = '敬老の日';
+    }
+
+    // スポーツの日の計算（10月の場合）
+    if ($month == '10' && $date == $secondMondayDate) {
+      $holidayName = 'スポーツの日';
+    }
+
+    if ($date == $springEquinoxDate) {
+      $holidayName = '春分の日';
+    } else if ($date == $autumnEquinoxDate) {
+      $holidayName = '秋分の日';
+    }
+
+    // 振替休日の判定
+    $prevDay = strtotime('-1 day', strtotime("$year-$month-$day"));
+    $prevDayOfWeek = date('w', $prevDay);
+    if ($prevDayOfWeek == 0 && isset($fixedHolidays[date('m-d', $prevDay)])) {
+      // 前日が日曜でかつ前日が祝日の場合は振替休日とする
+      $holidayName = '振替休日';
+    }
   }
 
-  $wd = getWeekday($date);
-  echo "{$date}は{$wd}です。";
+  return $holidayName;
+}
 
-  $firstDayOfMonth = date("w", mktime(0, 0, 0, $m, 1, $y));
-  $lastDayOfMonth = date("t", mktime(0, 0, 0, $m, 1, $y));
+if (isset($_POST['year']) && isset($_POST['month']) && isset($_POST['day'])) {
+  $y = $_POST['year'];
+  $m = $_POST['month'];
+  $d = $_POST['day'];
+
+  $result = calculateCalendar($y, $m, $d);
+
+  echo "<br>{$y}年{$m}月は{$result['totalDays']}日あります。";
+  echo "{$y}-{$m}-{$d}は{$result['weekday']}です。";
 
   // カレンダーの表示
   echo "<h2>{$y}年{$m}月</h2>";
 
   // 日にちと曜日の出力
-  for ($day = 1; $day <= $lastDayOfMonth; $day++) {
-    $currentDayOfWeek = date("w", mktime(0, 0, 0, $m, $day, $y));
-    $dayOfWeek = ['日', '月', '火', '水', '木', '金', '土'];
-
-    $holidayKey = "{$m}-{$day}";
-    $holidayName = isset($spHoliday[$holidayKey]) ? $spHoliday[$holidayKey] : '';
-
-    echo "{$day} ({$dayOfWeek[$currentDayOfWeek]}): {$holidayName}<br>";
+  foreach ($result['calendar'] as $dayInfo) {
+    echo $dayInfo['day'].'('.$dayInfo['weekday'].'):'. $dayInfo['holiday'].'<br>';
   }
 }
 ?>
